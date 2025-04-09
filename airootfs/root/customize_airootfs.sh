d77955ba417e85a7892a4c102b396ff1892269fa
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2025-04-09 03:20:24 -0400

    Update customize_airootfs.sh

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,1 +1,13 @@
+# cyrOS pre-Packing HOOK
 
+### SDDM THEME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+
+### Hyprland THEME
+git clone --depth=1 https://github.com/openai-ae/cyrDE.git dotfiles
+cd dotfiles
+./install.sh
+
+### CLEAN UP
+cd ..
+rm -rf dotfiles
