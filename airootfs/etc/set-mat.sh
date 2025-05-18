41a850f43bab38a9b24a820f9bc14dd5c24198a9
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2025-05-18 08:20:25 -0400

    Update set-mat.sh

--- airootfs/etc/set-mat.sh
+++ airootfs/etc/set-mat.sh
@@ -3,11 +3,7 @@
 # Check if this is the first boot (if a specific file does not exist)
 if [ ! -f /etc/.firstboot ]; then
     # Set the GTK theme (replace "Arc-Dark" with your custom theme)
-    gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
-
-    # Optionally set other themes (Icon theme, Cursor theme, etc.)
-    gsettings set org.gnome.desktop.interface icon-theme "Numix"
-    gsettings set org.gnome.desktop.interface cursor-theme "Adwaita"
+    gsettings set org.gnome.desktop.interface gtk-theme "custom-dark"
 
     # Mark that the first boot has been completed
     touch /etc/.firstboot
